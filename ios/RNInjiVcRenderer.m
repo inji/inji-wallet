05ec484bc5481a643cf4a189e44987cd299aba83
balachandarg-tw <balachandar.g@thoughtworks.com>
2025-09-14 20:47:19 +0530

    [INJIMOB-3499]: Updated Native module integration
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>

--- ios/RNInjiVcRenderer.m
+++ ios/RNInjiVcRenderer.m
@@ -3,7 +3,11 @@
 
 @interface RCT_EXTERN_MODULE(InjiVcRenderer, NSObject)
 
-RCT_EXTERN_METHOD(renderSvg:(NSString *)vcJsonString
+RCT_EXTERN_METHOD(init:(NSString *)traceabilityId)
+
+RCT_EXTERN_METHOD(renderVC:(NSString *)vcJsonString
+                  wellKnown:(NSString *)wellKnown
+                  credentialFormat:(NSString *)credentialFormat
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 
--- ios/RNInjiVcRenderer.swift
+++ ios/RNInjiVcRenderer.swift
@@ -1,29 +1,57 @@
 import Foundation
 import React
 import InjiVcRenderer
-
-
 @objc(InjiVcRenderer)
-class RNInjiVcRenderer: NSObject {
+class RNInjiVcRenderer: NSObject, RCTBridgeModule {
 
-  @objc static func requiresMainQueueSetup() -> Bool {
-    return false
-  }
-  
+    private var vcRenderer: InjiVcRenderer?
 
-  @objc(renderSvg:resolver:rejecter:)
-  func renderSvg(vcJsonString: String,
-                 resolve:  @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-    DispatchQueue.global(qos: .userInitiated).async {
-      do {
-        print("::::VcJson: \(vcJsonString)")
-        let renderer = InjiVcRenderer()
-        let results = renderer.renderSvg(vcJsonString: vcJsonString)
-        print("::::Rendered SVGs: \(results)")
-        resolve(results)
-      } catch {
-        reject("INJI_VC_RENDER_ERROR", "Failed to render SVG", error)
-      }
+    @objc static func requiresMainQueueSetup() -> Bool {
+        return false
     }
+
+    static func moduleName() -> String {
+        return "InjiVcRenderer"
+    }
+
+  @objc
+  func `init`(_ traceabilityId: String) {
+    vcRenderer = InjiVcRenderer(traceabilityId: traceabilityId)
+  }
+
+
+    @objc(renderVC:wellKnown:credentialFormat:resolver:rejecter:)
+    func renderVC(
+        credentialFormat: String,
+        wellKnown: String?,
+        vcJsonString: String,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let renderer = self.vcRenderer else {
+            reject(nil, "InjiVcRenderer not initialized", nil)
+            return
+        }
+
+        do {
+            let format = CredentialFormat.fromValue("test")
+            let result = try renderer.renderVC(
+                credentialFormat: format,
+                wellKnownJson: wellKnown,
+                vcJsonString: vcJsonString
+            )
+            resolve(result)
+        } catch {
+          rejectWithVcRendererError(error, reject: reject)
+        }
+    }
+
+  func rejectWithVcRendererError(_ error: Error, reject: RCTPromiseRejectBlock) {
+    if let vcRendererError = error as? VcRendererException {
+          reject(vcRendererError.errorCode, vcRendererError.message, vcRendererError)
+      } else {
+        let nsError = NSError(domain: error.localizedDescription, code: 0)
+        reject("ERR_UNKNOWN", nsError.localizedDescription, nsError)
+      }
   }
 }
