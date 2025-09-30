2e1e645b2cd7aaf439afa2c731a90151b74f8aa3
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-09-30 09:57:43 +0530

    [INJIMOB-3571] add: modify trusted verifiers in swift
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- ios/RNOpenID4VPModule.swift
+++ ios/RNOpenID4VPModule.swift
@@ -197,13 +197,12 @@ func sendErrorToVerifier(_ error: String, _ errorCode: String,
         throw NSError(domain: "OpenID4VP", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid Verifier data"])
       }
       
-      var clientMetadata: ClientMetadata? = nil
-      if let metadata = verifierDict["client_metadata"] as? [String: Any] {
-        let encoded = try JSONSerialization.data(withJSONObject: metadata)
-        clientMetadata = try ClientMetadata.deserializeAndValidate(clientMetadata: encoded)
+      var jwksUri: String? = nil
+      if let jwksUriInfo = verifierDict["jwks_uri"] as? String {
+        jwksUri = jwksUriInfo
       }
       
-      return Verifier(clientId: clientId, responseUris: responseUris,clientMetadata: clientMetadata)
+      return Verifier(clientId: clientId, responseUris: responseUris,jwksUri: jwksUri)
     }
   }
 
