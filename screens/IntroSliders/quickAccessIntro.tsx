02df71c89f52b0402fb9e3f2fbcac6678268623b
Vinay <vinayadattakavuluri@gmail.com>
2026-01-05 19:34:21 +0530

    fix: revert react-native-vector-icons to ^10.0.0
    Fixes CodeRabbit L2660785193 breaking change.
    
    Signed-off-by: Vinay KVD <vinayadattakavuluri@gmail.com>
    Signed-off-by: Vinay <vinayadattakavuluri@gmail.com>

--- screens/IntroSliders/quickAccessIntro.tsx
+++ screens/IntroSliders/quickAccessIntro.tsx
@@ -2,86 +2,89 @@ import React from 'react';
 import {Column, Text} from '../../components/ui';
 import {Theme} from '../../components/ui/styleUtils';
 import {useTranslation} from 'react-i18next';
-import {View, Image} from 'react-native';
+import {View, Image, Platform} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import {Platform} from 'react-native';
 
-const QRScannerComponent: React.FC = () => (
-  <Column crossAlign="center">
-    <Column style={Theme.CameraEnabledStyles.scannerContainer}>
-      <View
-        testID="qrScannerView"
-        style={[
-          Theme.CameraEnabledStyles.scannerContainer,
-          Theme.IntroSliderStyles.quickAccessIntroQrScanner,
-          {position: 'relative', overflow: 'visible'},
-        ]}>
-        <Image
-          testID="qrScannerImage"
-          source={require('../../assets/ClipPathGroup.png')}
-          style={{
-            width: '100%',
-            height: '100%',
-            resizeMode: 'cover',
-          }}
-        />
+const QRScannerComponent: React.FC = () => {
+  const {t} = useTranslation('ScanScreen');
+
+  return (
+    <Column crossAlign="center">
+      <Column style={Theme.CameraEnabledStyles.scannerContainer}>
         <View
-          style={{
-            position: 'absolute',
-            top: Theme.IntroSliderStyles.quickAccessIntroQrScanner.height / 4,
-            left: -12,
-            right: -12,
-            zIndex: 2,
-            overflow: 'visible',
-          }}>
+          testID="qrScannerView"
+          style={[
+            Theme.CameraEnabledStyles.scannerContainer,
+            Theme.IntroSliderStyles.quickAccessIntroQrScanner,
+            {position: 'relative', overflow: 'visible'},
+          ]}>
+          <Image
+            testID="qrScannerImage"
+            source={require('../../assets/ClipPathGroup.png')}
+            style={{
+              width: '100%',
+              height: '100%',
+              resizeMode: 'cover',
+            }}
+          />
           <View
             style={{
               position: 'absolute',
-              left: 0,
-              right: 0,
-              top: 1,
-              height: 5,
-              borderRadius: 3,
-              backgroundColor: Theme.Colors.linearGradientEnd + '26',
-              ...Platform.select({
-                ios: {
-                  shadowColor: Theme.Colors.linearGradientEnd,
-                  shadowOpacity: 0.2,
-                  shadowRadius: 3,
-                  shadowOffset: {width: 0, height: 1},
-                },
-                android: {
-                  elevation: 2,
-                },
-              }),
-            }}
-          />
-          <LinearGradient
-            colors={[
-              Theme.Colors.linearGradientStart,
-              Theme.Colors.linearGradientEnd,
-            ]}
-            style={{
-              position: 'absolute',
-              left: 0,
-              right: 0,
-              height: 3.5,
-              borderRadius: 2,
-            }}
-          />
+              top: Theme.IntroSliderStyles.quickAccessIntroQrScanner.height / 4,
+              left: -12,
+              right: -12,
+              zIndex: 2,
+              overflow: 'visible',
+            }}>
+            <View
+              style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 1,
+                height: 5,
+                borderRadius: 3,
+                backgroundColor: Theme.Colors.linearGradientEnd + '26',
+                ...Platform.select({
+                  ios: {
+                    shadowColor: Theme.Colors.linearGradientEnd,
+                    shadowOpacity: 0.2,
+                    shadowRadius: 3,
+                    shadowOffset: {width: 0, height: 1},
+                  },
+                  android: {
+                    elevation: 2,
+                  },
+                }),
+              }}
+            />
+            <LinearGradient
+              colors={[
+                Theme.Colors.linearGradientStart,
+                Theme.Colors.linearGradientEnd,
+              ]}
+              style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                height: 3.5,
+                borderRadius: 2,
+              }}
+            />
+          </View>
         </View>
-      </View>
-      <Column fill align="flex-start" style={{marginTop: 24}}>
-        <Text
-          testID="scanningGuideText"
-          align="center"
-          style={Theme.CameraEnabledStyles.holdPhoneSteadyText}>
-          {useTranslation('ScanScreen').t('scanningGuide')}
-        </Text>
+        <Column fill align="flex-start" style={{marginTop: 24}}>
+          <Text
+            testID="scanningGuideText"
+            align="center"
+            style={Theme.CameraEnabledStyles.holdPhoneSteadyText}>
+            {t('scanningGuide')}
+          </Text>
+        </Column>
       </Column>
     </Column>
-  </Column>
-);
+  );
+};
 
 export const StaticScanScreen: React.FC = () => {
   const {t} = useTranslation('ScanScreen');
