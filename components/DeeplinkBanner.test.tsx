a32dc6bc96e029b81a1de1acd03b168b7d99861d
abhip2565 <paul.apaul.abhishek.AP@gmail.com>
2026-05-29 18:16:11 +0530

    test: add snapshot tests for DeeplinkBanner
    Signed-off-by: abhip2565 <paul.apaul.abhishek.AP@gmail.com>

--- components/DeeplinkBanner.test.tsx
+++ components/DeeplinkBanner.test.tsx
@@ -11,14 +11,20 @@ jest.mock('./BannerNotificationController', () => ({
   })),
 }));
 
-jest.mock('./BannerNotification', () => ({
-  BannerNotification: jest.fn(() => null),
-  BannerStatusType: {
-    IN_PROGRESS: 'inProgress',
-    SUCCESS: 'success',
-    ERROR: 'error',
-  },
-}));
+jest.mock('./BannerNotification', () => {
+  const React = require('react');
+  const {View} = require('react-native');
+  return {
+    BannerNotification: jest.fn(() =>
+      React.createElement(View, {testID: 'mock-banner'}),
+    ),
+    BannerStatusType: {
+      IN_PROGRESS: 'inProgress',
+      SUCCESS: 'success',
+      ERROR: 'error',
+    },
+  };
+});
 
 const {BannerNotification} = require('./BannerNotification');
 const {UseBannerNotification} = require('./BannerNotificationController');
@@ -36,6 +42,24 @@ describe('DeeplinkBanner Component', () => {
 
   it('should match snapshot with default props', () => {
     const {toJSON} = render(<DeeplinkBanner />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should match snapshot when busy in inline mode', () => {
+    (UseBannerNotification as jest.Mock).mockReturnValueOnce({
+      ...defaultBannerState,
+      isCredentialOfferDroppedBusy: true,
+    });
+    const {toJSON} = render(<DeeplinkBanner />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should match snapshot when busy in absolute mode', () => {
+    (UseBannerNotification as jest.Mock).mockReturnValueOnce({
+      ...defaultBannerState,
+      isCredentialOfferDroppedBusy: true,
+    });
+    const {toJSON} = render(<DeeplinkBanner absolute />);
     expect(toJSON()).toMatchSnapshot();
   });
 
