5c73dfd0254729c6d1cd49a3f8f74fab0828ca42
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-07-03 14:31:09 +0530

    refactor: remove inji test automation
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- injitest/src/main/java/inji/exceptions/PlatformNotSupportException.java
+++
@@ -1,7 +0,0 @@
-package inji.exceptions;
-
-public class PlatformNotSupportException extends RuntimeException {
-    public PlatformNotSupportException(String message) {
-        super(message);
-    }
-}
